0e96ba2b5ad9559008e5f03fcd8ea29aeac765fc
shejiewei <2284872818@qq.com>
2019-01-23 10:58:16 +0800

    sjw

---
+++ java/src/main/java/spi/Bumblebee.java
@@ -0,0 +1,12 @@
+package spi;
+
+/**
+ * Created by shejiewei on 2019/1/23.
+ */
+public class Bumblebee implements Robot {
+
+
+    public void sayHello() {
+        System.out.println("Hello, I am Bumblebee.");
+    }
+}
