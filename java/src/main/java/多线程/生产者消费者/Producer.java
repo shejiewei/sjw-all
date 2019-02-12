5673c370d7f28cedb95bc468ed69b0b694d08f4a
shejiewei <2284872818@qq.com>
2019-02-12 12:42:40 +0800

    shejiewei

---
+++ java/src/main/java/多线程/生产者消费者/Producer.java
@@ -0,0 +1,46 @@
+package 多线程.生产者消费者;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+        import java.util.Random;
+        import java.util.concurrent.BlockingQueue;
+        import java.util.concurrent.TimeUnit;
+        import java.util.concurrent.atomic.AtomicInteger;
+
+
+
+public class Producer implements Runnable {
+    private volatile boolean isRunning = true;
+    private BlockingQueue<PCData> queue;// 内存缓冲区
+    private static AtomicInteger count = new AtomicInteger();// 总数 原子操作
+    private static final int SLEEPTIME = 1000;
+
+    public Producer(BlockingQueue<PCData> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        PCData data = null;
+        Random r = new Random();
+        System.out.println("start producting id:" + Thread.currentThread().getId());
+        try {
+            while (isRunning) {
+                Thread.sleep(r.nextInt(SLEEPTIME));
+                data = new PCData(count.incrementAndGet());
+                System.out.println(data + " 加入队列");
+                if (!queue.offer(data, 2, TimeUnit.SECONDS)) {
+                    System.err.println(" 加入队列失败");
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+}
