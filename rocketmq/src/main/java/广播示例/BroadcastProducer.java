5cd94dbf206cb588c23f863fe69336dcf9428d35
shejiewei <2284872818@qq.com>
2019-03-08 20:12:32 +0800

    sjw

---
+++ rocketmq/src/main/java/广播示例/BroadcastProducer.java
@@ -0,0 +1,26 @@
+package 广播示例;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * Created by Administrator on 2019/3/8.
+ */
+public class BroadcastProducer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
+        producer.start();
+
+        for (int i = 0; i < 100; i++){
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    "OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
