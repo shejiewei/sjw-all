793fa2b3f0e3a2109901b5e677b24836e49b7afb
shejiewei <2284872818@qq.com>
2019-04-29 11:05:47 +0800

    sjw

---
+++ java/src/main/java/java细节大全/传入字符串做条件判断/test.java
@@ -0,0 +1,20 @@
+package java细节大全.传入字符串做条件判断;
+
+/**
+ * Created by shejiewei on 2019/4/29.
+ */
+public class test {
+
+     static String ss="a>0&&a!=-999.9f&&a!=999.9f";
+     public static void main(String[] args) {
+         //javaassist
+           float a= 33.0f;
+           if (a>0&&a!=-999.9f&&a!=999.9f){
+
+               System.out.println("true");
+           }
+
+
+        }
+
+}
