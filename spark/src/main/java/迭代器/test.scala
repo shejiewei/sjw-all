39ecdd1cb2007f284245421500ab24e6212917d3
shejiewei <2284872818@qq.com>
2019-05-27 15:53:43 +0800

    sjw

---
+++ spark/src/main/java/迭代器/test.scala
@@ -0,0 +1,17 @@
+package 迭代器
+
+/**
+  * Created by shejiewei on 2019/5/27.
+  */
+object test {
+
+
+  def main(args: Array[String]): Unit = {
+
+     val ita=Iterator(2,3,4,77,5,46,44)
+     val itb=Iterator(2,3,4,77,5,46,443)
+
+     println("ita.size"+ita.size)
+
+  }
+}
