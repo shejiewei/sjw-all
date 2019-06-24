d83179256f32ac05df41c606b0a0533cf2aeed2c
shejiewei <2284872818@qq.com>
2019-06-24 15:57:57 +0800

    sjw

---
+++ java/src/main/java/java细节大全/string动态加载执行/test.java
@@ -0,0 +1,44 @@
+package java细节大全.string动态加载执行;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by shejiewei on 2019/6/24.
+ */
+public class test {
+
+
+
+    /**
+     * 动态加载方法
+     * @author wangyfc
+     *
+     */
+
+    /*    public static Object executeExpression(String jexlExp,Map<String,Object> map){
+            JexlEngine jexl=new JexlEngine();
+            Expression e = jexl.createExpression(jexlExp);
+            JexlContext jc = new MapContext();
+            for(String key:map.keySet()){
+                jc.set(key, map.get(key));
+            }
+            if(null==e.evaluate(jc)){
+                return "";
+            }
+            return e.evaluate(jc);
+        }
+*/
+
+         public static void main(String[] args) {
+
+                 Map<String, Object> map = new HashMap<>();
+                 map.put("alive", "coding every day");
+                 map.put("out", System.out);
+                 String expression = "out.print(alive)";
+               //  executeExpression(expression, map);
+                 // ReflectUtils.executeExpression(expression, map);
+
+         }
+
+}
