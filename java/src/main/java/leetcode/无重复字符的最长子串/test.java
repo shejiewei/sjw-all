8317e53ea5846e94e584a396175028ebbee5d1a0
shejiewei <2284872818@qq.com>
2019-12-04 17:32:38 +0800

    sjw

---
+++ java/src/main/java/leetcode/无重复字符的最长子串/test.java
@@ -0,0 +1,17 @@
+package leetcode.无重复字符的最长子串;
+
+/**
+ * Created by shejiewei on 2019/12/4.
+ */
+public class test
+{
+     public static void main(String[] args) {
+
+
+      }
+    public int lengthOfLongestSubstring(String s) {
+
+         return 1;
+    }
+
+}
