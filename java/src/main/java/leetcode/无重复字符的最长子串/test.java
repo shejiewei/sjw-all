91992100d9b31eda9a3bbe1c34f0bb8438aa0a06
shejiewei <2284872818@qq.com>
2019-12-05 09:36:48 +0800

    sjw

--- java/src/main/java/leetcode/无重复字符的最长子串/test.java
+++ java/src/main/java/leetcode/无重复字符的最长子串/test.java
@@ -1,5 +1,8 @@
 package leetcode.无重复字符的最长子串;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by shejiewei on 2019/12/4.
  */
@@ -7,11 +10,32 @@ public class test
 {
      public static void main(String[] args) {
 
-
+         lengthOfLongestSubstring("abccdabcd");
       }
-    public int lengthOfLongestSubstring(String s) {
-
-         return 1;
+    public static int lengthOfLongestSubstring(String s) {
+        Set digits=new HashSet<Character>();
+        int result = 0;
+        int startIndex = 0;
+        char[] ss=s.toCharArray();
+        for (int i=0;i<s.length();i++)
+        {
+            if (!digits.contains(ss[i])){
+                digits.add(ss[i]);
+            }else{
+                if (result<digits.size()){
+                    result = digits.size();
+                }
+                while (ss[startIndex]!=ss[i]){
+                    digits.remove(ss[startIndex]);
+                    startIndex++;
+                }
+                startIndex++;
+            }
+        }
+        if (result<digits.size()){
+            result = digits.size();
+        }
+        return result;
     }
 
 }
