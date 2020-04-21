cab85349d3729164ba3d394cb8b8d12bcd3217c8
shejiewei <2284872818@qq.com>
2020-04-21 15:54:50 +0800

    sjw

--- flink/src/main/java/sql/FilnkCostKafka.java
+++ flink/src/main/java/sql/FilnkCostKafka.java
@@ -1,24 +1,26 @@
 package sql;
+
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
 
 import java.util.Properties;
+
 /**
  * Created by shejiewei on 2020/4/21.
  */
 public class FilnkCostKafka {
     public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        /*final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(1000);
 
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "192.168.93.3:9092");
-        properties.setProperty("zookeeper.connect", "192.168.93.3:2181");
+        properties.setProperty("bootstrap.servers", "192.168.93.6:9092");
+        properties.setProperty("zookeeper.connect", "192.168.93.6:2181");
         properties.setProperty("group.id", "test-consumer-group");
         FlinkKafkaConsumer<String> myConsumer=new FlinkKafkaConsumer<String>("test", new SimpleStringSchema(), properties);
         //FlinkKafkaConsumer09<String> myConsumer = new FlinkKafkaConsumer09<String>("test", new SimpleStringSchema(), properties);
@@ -28,7 +30,23 @@ public class FilnkCostKafka {
 
         counts.print();
 
-        env.execute("WordCount from Kafka data");
+        env.execute("WordCount from Kafka data");*/
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //@TODO checkpoint配置
+        //@TODO 设置statebackend
+        String topic = "test";
+        Properties prop = new Properties();
+        prop.setProperty("bootstrap.servers","192.168.93.6:9092");
+        prop.setProperty("group.id","test-consumer-group");
+
+        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<String>(topic, new SimpleStringSchema(), prop);
+        //设置消费策略
+        consumer.setStartFromGroupOffsets();
+        DataStreamSource<String> text = env.addSource(consumer);
+        //将并行度设置为1
+        text.print().setParallelism(1);
+        env.execute("KafkaSourceExample");
     }
 
     public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
