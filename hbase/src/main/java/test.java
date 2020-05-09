824ff1c8a68870a5572b235297e156abb6c1fb32
shejiewei <2284872818@qq.com>
2020-05-09 15:33:05 +0800

    sjw

--- hbase/src/main/java/test.java
+++ hbase/src/main/java/test.java
@@ -4,15 +4,13 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
 /**
- * Created by shejiewei on 2019/3/31.
+ *
  */
 public class test {
 
@@ -21,20 +19,11 @@ public class test {
 
     @Before
     public void init() throws IOException {
-
-//        configuration.set("hbase.rootdir","hdfs://192.168.93.131:8020/hbase");
-//        configuration.set("hbase.zookeepr.quorum","192.168.93.131:2181");
         conf = HBaseConfiguration.create();
         conf.set("hbase.zookeeper.quorum", "192.168.93.131");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
         conf.set("hbase.master", "192.168.93.131:60000");
-//        configuration.set("hbase.zookeeper.property.clientPort", "2181");
-//        configuration.set("hbase.zookeeper.quorum", "192.168.93.131");
-//        configuration.set("hbase.master", "192.168.93.131:6000");
-        //connection= ConnectionFactory.createConnection(conf);
         connection=ConnectionFactory.createConnection(conf);
-
-
     }
     @Test
     public void isExit() throws IOException {
