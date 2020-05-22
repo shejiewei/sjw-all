e678f7352a3a0470ddc75cbc9d99746433e9c3a7
shejiewei <2284872818@qq.com>
2020-05-22 11:47:19 +0800

    sjw

---
+++ flink/src/main/java/calcite/CsvSchemaFactory.java
@@ -0,0 +1,25 @@
+package calcite;
+
+/**
+ * Created by shejiewei on 2020/5/20.
+ */
+
+import org.apache.calcite.schema.Schema;
+import org.apache.calcite.schema.SchemaFactory;
+import org.apache.calcite.schema.SchemaPlus;
+
+import java.util.Map;
+
+public class CsvSchemaFactory implements SchemaFactory {
+
+    /**
+     * parentSchema 他的父节点，一般为root
+     * name     数据库的名字，它在model中定义的
+     * operand  也是在mode中定义的，是Map类型，用于传入自定义参数。
+     * */
+    @Override
+    public Schema create(SchemaPlus parentSchema, String name, Map<String, Object> operand) {
+        //return new CsvSchema(String.valueOf(operand.get("dataFile")));
+        return null;
+    }
+}
---
+++ flink/src/main/java/calcite/ReourceUtil.java
@@ -0,0 +1,10 @@
+package calcite;
+
+/**
+ * Created by shejiewei on 2020/5/20.
+ */
+public class ReourceUtil {
+    public static String getResourceAsString(String s) {
+        return null;
+    }
+}
